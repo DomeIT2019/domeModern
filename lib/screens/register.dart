c418aa33488fc752a341add8615780a8de89a6a8
DomeIT <bestcsv@gmail.com>
2019-08-03 15:56:36 +0700

    Finish 3 AUG

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -8,65 +8,92 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
 // Explicit
   Color myBlue = Color.fromARGB(255, 239, 108, 147);
+  final formKey = GlobalKey<FormState>();
+  String nameString, emailString, passwordString;
 
   // Method
   Widget nameText() {
     return TextFormField(
-      style: TextStyle(color: Colors.orange[800]),
+      style: TextStyle(color: myBlue),
       decoration: InputDecoration(
-        enabledBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: Colors.orange[800])),
+        enabledBorder:
+            UnderlineInputBorder(borderSide: BorderSide(color: myBlue)),
         icon: Icon(
           Icons.account_box,
           size: 36.0,
-          color: Colors.orange[800],
+          color: myBlue,
         ),
         labelText: 'Name :',
-        labelStyle: TextStyle(color: Colors.orange[800]),
+        labelStyle: TextStyle(color: myBlue),
         helperText: 'Type Display Name',
-        helperStyle: TextStyle(color: Colors.orange[800]),
+        helperStyle: TextStyle(color: myBlue),
         hintText: 'English Only',
       ),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Please Fill Name in Blank';
+        }
+      },
+      onSaved: (String value) {
+        nameString = value;
+      },
     );
   }
 
   Widget emailText() {
     return TextFormField(
-      style: TextStyle(color: Colors.orange[800]),
+      keyboardType: TextInputType.emailAddress,
+      style: TextStyle(color: myBlue),
       decoration: InputDecoration(
-        enabledBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: Colors.orange[800])),
+        enabledBorder:
+            UnderlineInputBorder(borderSide: BorderSide(color: myBlue)),
         icon: Icon(
           Icons.email,
           size: 36.0,
-          color: Colors.orange[800],
+          color: myBlue,
         ),
         labelText: 'Email :',
-        labelStyle: TextStyle(color: Colors.orange[800]),
+        labelStyle: TextStyle(color: myBlue),
         helperText: 'Type Yoyr Email',
-        helperStyle: TextStyle(color: Colors.orange[800]),
+        helperStyle: TextStyle(color: myBlue),
         hintText: 'you@demo.com',
       ),
+      validator: (String value) {
+        if (!((value.contains('@')) && (value.contains('.')))) {
+          return 'Keep Email Format you@demo.com';
+        }
+      },
+      onSaved: (String value) {
+        emailString = value;
+      },
     );
   }
 
   Widget passwordText() {
     return TextFormField(
-      style: TextStyle(color: Colors.orange[800]),
+      style: TextStyle(color: myBlue),
       decoration: InputDecoration(
-        enabledBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: Colors.orange[800])),
+        enabledBorder:
+            UnderlineInputBorder(borderSide: BorderSide(color: myBlue)),
         icon: Icon(
           Icons.lock,
           size: 36.0,
-          color: Colors.orange[800],
+          color: myBlue,
         ),
         labelText: 'Password :',
-        labelStyle: TextStyle(color: Colors.orange[800]),
+        labelStyle: TextStyle(color: myBlue),
         helperText: 'Type Your Password',
-        helperStyle: TextStyle(color: Colors.orange[800]),
+        helperStyle: TextStyle(color: myBlue),
         hintText: '6-8 Digit',
       ),
+      validator: (String value) {
+        if (value.length < 6) {
+          return 'More 6 Digit For Password';
+        }
+      },
+      onSaved: (String value) {
+        passwordString = value;
+      },
     );
   }
 
@@ -75,6 +102,11 @@ class _RegisterState extends State<Register> {
       icon: Icon(Icons.cloud_upload),
       onPressed: () {
         print('Your Click Upload');
+        if (formKey.currentState.validate()) {
+          formKey.currentState.save();
+          print(
+              'name = $nameString, email = $emailString, password = $emailString');
+        }
       },
     );
   }
@@ -94,12 +126,15 @@ class _RegisterState extends State<Register> {
         child: Container(
           margin: EdgeInsets.only(top: 24.0),
           width: 300.0,
-          child: Column(
-            children: <Widget>[
-              nameText(),
-              emailText(),
-              passwordText(),
-            ],
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: <Widget>[
+                nameText(),
+                emailText(),
+                passwordText(),
+              ],
+            ),
           ),
         ),
       ),
